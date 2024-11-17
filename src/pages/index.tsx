c4d420d5377e042890146149d314f3d1cd31a0b3
윤강석 <151100068+seokseok9773@users.noreply.github.com>
2024-11-18 00:11:37 +0900

    inferServerSidePropsType코드 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,27 @@ import style from "./index.module.css";
 import SearchableLayout from "@/components/searchable-layout";
 import books from "@/Mock/books.json";
 import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType } from "next";
 
 /* 
 getServerSideProps 라는 Next의 예약어를 사용하면 해당 코드가 있는 페이지는 
 서버 사이드 렌더링으로 동작하게 된다.
 */
-export const getServerSideProps = () => {};
+export const getServerSideProps = () => {
+  // 컴포넌트보다 먼저 실행되어, 컴포넌트에 필요한 데이터를 불러오는 함수
+  // 1. 오직 서버측에서만 실행되는 함수
+  const data = "hello";
+  return {
+    props: {
+      data,
+    },
+  };
+};
 
-export default function Home() {
+export default function Home({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(data);
   return (
     <div className={style.container}>
       <section>
