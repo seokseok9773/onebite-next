43f7df1998e295e2afd6693a4a4d20c581179c0d
윤강석 <151100068+seokseok9773@users.noreply.github.com>
2024-11-20 02:56:29 +0900

    검색 기능 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,53 @@
 import { ReactNode } from "react";
 import style from "./index.module.css";
 import SearchableLayout from "@/components/searchable-layout";
-import books from "@/Mock/books.json";
+// import books from "@/Mock/books.json";
 import BookItem from "@/components/book-item";
+import fetchBooks from "@/lib/fetch-books";
+import { InferGetServerSidePropsType } from "next";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 
 /* 
 getServerSideProps 라는 Next의 예약어를 사용하면 해당 코드가 있는 페이지는 
 서버 사이드 렌더링으로 동작하게 된다.
 */
-export const getServerSideProps = () => {};
+export const getServerSideProps = async () => {
+  // 직렬 구조로 데이터를 불러오는 형식
+  // const allBooks = await fetchBooks();
+  // const recoBooks = await fetchRandomBooks();
 
-export default function Home() {
+  // allBooks 와 rocoBooks를 병렬형태로 가져오기
+  // Promise라는 메서드는 인수로 전달한 배열안의 모든 비동기 함수를 실행시켜줌
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
+
+  return {
+    props: {
+      allBooks,
+      recoBooks,
+    },
+  };
+};
+
+export default function Home({
+  allBooks,
+  recoBooks,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log("allBooks", allBooks);
+  console.log("recoBooks", recoBooks);
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {recoBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,11 +1,25 @@
 import SearchableLayout from "@/components/searchable-layout";
 // import { useRouter } from "next/router";
 import { ReactNode } from "react";
-import books from '@/Mock/books.json';
 import BookItem from "@/components/book-item";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
 
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const q = context.query.q;
+  const books = await fetchBooks(q as string);
+  return {
+    props: {
+      books,
+    },
+  };
+};
 
-export default function Page() {
+export default function Page({
+  books,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   // 쿼리 스트링으로 주소에서 값을 가져오기 위해 useRouter 훅 사용
   // const router = useRouter();
   // const { q }= router.query;
@@ -13,12 +27,12 @@ export default function Page() {
   return (
     <div>
       {books.map((book) => (
-        <BookItem key={book.id} {...book}/>
+        <BookItem key={book.id} {...book} />
       ))}
     </div>
   );
 }
 
 Page.getLayout = (page: ReactNode) => {
-  return <SearchableLayout>{page}</SearchableLayout>
-}
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
