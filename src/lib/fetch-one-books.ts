4dfbd6ad0897a734799740682eafb68401d6483b
윤강석 <151100068+seokseok9773@users.noreply.github.com>
2024-11-24 22:49:12 +0900

    ISR방식 추가 / meta태그 설정

--- src/lib/fetch-one-books.ts
+++ src/lib/fetch-one-books.ts
@@ -2,7 +2,7 @@ import { BookData } from "@/types";
 
 export default async function fetchOneBooks(id: number): Promise<BookData | null> {
 
-  const url = `http://localhost:12345/book/${id}`
+  const url = `https://onebite-books-server-main-gilt.vercel.app/book/${id}`
 
   try {
     const response = await fetch(url);
--- src/lib/fetch-random-books.ts
+++ src/lib/fetch-random-books.ts
@@ -1,7 +1,7 @@
 import type { BookData } from "@/types";
 
 export default async function FetchRandomBooks(): Promise<BookData[]> {
-  const url = 'http://localhost:12345/book/random';
+  const url = 'https://onebite-books-server-main-gilt.vercel.app/book/random';
 
   try {
     const response = await fetch(url)
